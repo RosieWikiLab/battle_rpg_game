a2e2eddcc25634549b35bb6baa5d2c095f5efef7
ilpyoyang <ilpyoyang@gmail.com>
2025-03-17 10:51:24 +0900

    필수1 = 파일로부터 데이터 읽어오기 기능

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
 void main() {
-  
+  // 필수1 = 파일로부터 데이터 읽어오기 기능
+  var characterFile = File('../lib/characters.txt');
+  var monsterFile = File('../lib/monsters.txt');
+  // print(characterFile.readAsStringSync());
 }
